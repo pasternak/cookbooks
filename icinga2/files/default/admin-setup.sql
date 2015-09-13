21b3af4bcca6c98855fd892f53671e55546def79
Karol Pasternak <karol@pasternak.pro>
2015-09-13 18:02:44 +0100

    Splitting cookbook repo over seperated repositories

--- icinga2/files/default/admin-setup.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `icingaweb_user` VALUES ('admin',1,'$1$9Á\Zsµ(4@$.Bht2mmGDohLuZZoOSQlu.','2014-12-01 17:33:06',NULL);
--- mariadb/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: mariadb
-# Recipe:: default
-#
-# Copyright (C) 2014 Karol Pasternak
-#
-# All rights reserved - Do Not Redistribute
-#
-
